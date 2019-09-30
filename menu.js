327d5cb7ed0b936010c8402a7f82b93973ebce67
Ali Churcher <churcher.ali@gmail.com>
2019-10-01 11:35:58 +1300

    add keyboard navigation to menu

--- menu.js
+++ menu.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+    initHamburgerMenu();
+    initKeyboardNavigation();
+}
+
+function initKeyboardNavigation() {
+    document.querySelectorAll('.nav-item').forEach((navitem) => navitem.addEventListener('keydown', function(e) {
+        if (e.key == 'Enter' || e.keyCode == 13) {
+            let destination = e.target.getElementsByTagName('a')[0].click();
+        }
+    }));
+}
+
+function initHamburgerMenu() {
     let menuToggle = document.getElementById("js-menu-toggle");
     let menu = document.getElementById("js-nav-items");
-
     menuToggle.onclick = () => {
         menu.classList.toggle('is-active');
         menuToggle.setAttribute("aria-expanded", true);
-    }
-
+    };
 }
